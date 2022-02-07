b39837310bf51378e07ca0151dc9547bd4f5a5fb
rotichknovestus <noverotich@gmail.com>
2022-02-07 20:40:55 +0300

    Adopt default vpc and rename tf file to prod

---
+++ prod.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_s3_bucket" "prod_tf_course" {
+  bucket = "tf-course-20220205"
+  acl    = "private"
+}
+
+resource "aws_default_vpc" "default" {
+
+
+}
