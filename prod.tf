29ca1e6ecb430a07eb02714bce39d9a8ab96a6a6
rotichknovestus <noverotich@gmail.com>
2022-02-07 22:31:51 +0300

    Added AWS Instance  with ami and instance type

--- prod.tf
+++ prod.tf
@@ -34,8 +34,22 @@ resource "aws_security_group" "prod_web" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"
+    ]
   }
+  tags = {
+    "Terraform" : "true"
+  }
+
+}
+
+resource "aws_instance" "prod_web" {
+  ami = "ami-0c4679e50e254f385"
+  instance_type = "t2.nano"
+
+  vpc_security_group_ids = [
+    aws_security_group.prod_web.id
+  ] 
 
   tags = {
     "Terraform" : "true"
