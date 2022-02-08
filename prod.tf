e658c1be20c071c30bdada9e30518fe6b30273a8
“kimpetertanui” <kimelipeter526@gmail.com>
2022-02-08 22:26:39 +0300

    Refactored to use sg

--- prod.tf
+++ prod.tf
@@ -2,28 +2,33 @@ provider "aws" {
   profile = "default"
   region  = "us-west-2"
 }
-
 resource "aws_s3_bucket" "prod_tf_course" {
   bucket = "tf-course-20220205"
   acl    = "private"
 }
-
 resource "aws_default_vpc" "default" {
-
-
 }
-#hello
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "us-west-2a"
+   tags = {
+    "Terraform" : "true"
+  }
+}
+resource "aws_default_subnet" "default_az2" {
+  availability_zone = "us-west-2b"
+  tags = {
+    "Terraform" : "true"
+  }
+}
 resource "aws_security_group" "prod_web" {
   name        = "prod_web"
   description = "Allow standard http and https ports inbound and outbound and everythong outbound"
-
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
     from_port   = 443
     to_port     = 443
@@ -31,37 +36,59 @@ resource "aws_security_group" "prod_web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
     cidr_blocks = ["0.0.0.0/0"
     ]
   }
   tags = {
     "Terraform" : "true"
   }
-
 }
 
-resource "aws_instance" "prod_web" {
-  ami = "ami-0c4679e50e254f385"
-  instance_type = "t2.nano"
 
-  vpc_security_group_ids = [
-    aws_security_group.prod_web.id
-  ] 
+resource "aws_elb" "prod_web" {
+  name            = "prod-web"
+  subnets         = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  security_groups = [aws_security_group.prod_web.id]
 
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
   tags = {
     "Terraform" : "true"
   }
-
 }
-
-resource "aws_eip" "prod_web" {
-  instance = aws_instance.prod_web.id
-
+resource "aws_launch_template" "prod_web" {
+  name_prefix   = "prod-web"
+  image_id      = "ami-0c4679e50e254f385"
+  instance_type = "t2.micro"
   tags = {
     "Terraform" : "true"
   }
+}
+resource "aws_autoscaling_group" "prod_web" {
+  availability_zones  = ["us-west-2a", "us-west-2b"]
+  vpc_zone_identifier = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  desired_capacity    = 1
+  max_size            = 1
+  min_size            = 1
 
+  launch_template {
+    id      = aws_launch_template.prod_web.id
+    version = "$Latest"
+  }
+  tag {
+    key                 = "Terraform"
+    value               = "true"
+    propagate_at_launch = true
+  }
+}
+resource "aws_autoscaling_attachment" "prod_web" {
+  autoscaling_group_name = aws_autoscaling_group.prod_web.id
+  elb                    = aws_elb.prod_web.id
 }
